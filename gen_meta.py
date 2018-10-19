3f1754ea87075ea3aee962043f69852887da92b5
Nicholas Bishop <nicholasbishop@gmail.com>
2018-10-19 10:12:48 -0400

    Improve compat with older python

--- gen_meta.py
+++ gen_meta.py
@@ -91,14 +91,14 @@ def main():
         src.add('from pumpkinpy.types import Function, Param, Type')
         src.add('FUNCTIONS = (')
         for func in functions:
-            src.add(f'  {func},')
+            src.add('  {},'.format(func))
         src.add(')')
         src.add('ENUMS = {')
         for key, value in enums.items():
-            src.add(f'  \'{key}\': {value},')
+            src.add("  '{}': {}".format(key, value))
         src.add('}')
         for key, value in enums.items():
-            src.add(f'{key} = {value}')
+            src.add('{} = {}'.format(key, value))
         wfile.write(src.text())
 
 
