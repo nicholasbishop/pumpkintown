43817490b512950cf12d5f139d64069eeb8334bd
Nicholas Bishop <nbishop@neverware.com>
2018-10-19 17:11:27 -0400

    tmp

--- pumpkintown_custom_trace.cc
+++ pumpkintown_custom_trace.cc
@@ -28,4 +28,27 @@ void trace_append_glLinkProgram(const GLuint program) {
   }
 }
 
+void trace_append_glEGLImageTargetTexture2DOES(GLenum target, GLeglImageOES image) {
+  const GLint level{0};
+  GLint internalformat;
+  GLsizei width{0};
+  GLsizei height{0};
+  GLint border;
+  GLenum format;
+  GLenum type;
+  const GLvoid * data;
+
+  // Bind to the current texture target (TODO, do we need to create a new texture?)
+  using Fn = void (*)(GLenum, GLeglImageOES);
+  static Fn real_fn = reinterpret_cast<Fn>(get_real_proc_addr("glEGLImageTargetTexture2DOES"));
+  real_fn(GL_TEXTURE_2D, image);
+
+  glGetTexLevelParameteriv(GL_TEXTURE_2D, level, GL_TEXTURE_WIDTH, &width);
+  glGetTexLevelParameteriv(GL_TEXTURE_2D, level, GL_TEXTURE_HEIGHT, &height);
+  fprintf(stderr, "%d x %d\n", width, height);
+
+  // glTexImage2D(target2, level, internalformat, width, height, border,
+  //              format, type, data);
+}
+
 }
--- pumpkintown_custom_trace.hh
+++ pumpkintown_custom_trace.hh
@@ -7,6 +7,8 @@ namespace pumpkintown {
 
 void trace_append_glLinkProgram(GLuint program);
 
+void trace_append_glEGLImageTargetTexture2DOES(GLenum target, GLeglImageOES image);
+
 }
 
 #endif  // PUMPKINTOWN_CUSTOM_TRACE_HH_
